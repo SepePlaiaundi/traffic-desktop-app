3fee50047ef3e11d6ad8bd427328d22447ac8470
ikebg-salmaneaaboud <ikebg@plaiaundi.net>
2026-01-09 10:59:36 +0100

    added the the line chart

---
+++ Controls/DashboardLineChart.xaml.cs
@@ -0,0 +1,35 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TrafficDesktopApp.Controls
+{
+    public partial class DashboardLineChart : UserControl
+    {
+        public SeriesCollection Series { get; set; }
+        public string[] Labels { get; set; }
+
+        public DashboardLineChart()
+        {
+            InitializeComponent();
+
+            Labels = new[] { "23 h", "4", "9", "14", "3", "4", "5", "30" };
+
+            Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = new ChartValues<int> { 0, 1, 2, 4, 6, 5, 7, 10 },
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 3,
+                    Fill = Brushes.Transparent,
+                    PointGeometry = null, // sin puntos
+                    LineSmoothness = 0    // línea recta, no curva
+                }
+            };
+
+            DataContext = this;
+        }
+    }
+}
